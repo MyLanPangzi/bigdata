0475aa51b89bd4fad14965fd98183d2329064390
1251723871@qq.com <smailXie@163.com>
2020-12-06 19:54:13 +0800

    refactor(flink): remove redis-connector

--- flink/realtime-analysis/src/main/scala/com/hiscat/realtime/analysis/connector/redis/RedisOptions.scala
+++
@@ -1,8 +0,0 @@
-package com.hiscat.realtime.analysis.connector.redis
-
-case class RedisOptions(
-                         host: String,
-                         port: Int,
-                         mapKey: String,
-                         keyDelimiter: String
-                       )
